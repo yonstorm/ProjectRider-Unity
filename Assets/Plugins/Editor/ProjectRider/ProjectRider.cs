a67e57eaa94ac0316233e492c51e7b0017af1363
yonstorm <juv@users.noreply.github.com>
2016-03-09 10:44:13 +0200

    rider integration functionality stubs

---
+++ Assets/Plugins/Editor/ProjectRider/ProjectRider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace ChaosModel.ProjectRider{
+	[InitializeOnLoad]
+	public static class ProjectRider{
+		static ProjectRider(){
+			Debug.Log("Rider integration initializing");
+		}
+
+		[UnityEditor.Callbacks.OnOpenAsset]
+		private static bool OnAssetOpened(int instanceId, int line){
+			return false;
+		}
+
+
+
+	}
+}
