550f50f3ab13023d0b4eb639353e44487e469c90
yonstorm <juv@users.noreply.github.com>
2016-03-09 12:36:03 +0200

    Code reformatting

--- Assets/Plugins/Editor/ProjectRider/ProjectValidator.cs
+++ Assets/Plugins/Editor/ProjectRider/ProjectValidator.cs
@@ -5,97 +5,111 @@ using System.Xml.Linq;
 
 namespace ChaosModel.ProjectRider
 {
-	internal sealed class ProjectValidator{
-		private string projectDirectory;
-		private string _productName;
-		private string _targetVersion;
+    internal sealed class ProjectValidator
+    {
+        private string projectDirectory;
+        private string _productName;
+        private string _targetVersion;
 
-		internal string SolutionFile{
-			get { return projectDirectory + Path.DirectorySeparatorChar + _productName + ".sln"; }
-		}
+        internal string SolutionFile
+        {
+            get { return projectDirectory + Path.DirectorySeparatorChar + _productName + ".sln"; }
+        }
 
-		public ProjectValidator(string projectDirectory, string productName, string targetVersion){
-			this.projectDirectory = projectDirectory;
-			this._productName = productName;
-			this._targetVersion = targetVersion;
-		}
+        public ProjectValidator(string projectDirectory, string productName, string targetVersion)
+        {
+            this.projectDirectory = projectDirectory;
+            this._productName = productName;
+            this._targetVersion = targetVersion;
+        }
 
-		public bool Validate(){
-			return ValidateProjectFiles() && ValidateDotSettings() && ValidateDebugSettings();
-		}
+        public bool Validate()
+        {
+            return ValidateProjectFiles() && ValidateDotSettings() && ValidateDebugSettings();
+        }
 
-		private bool ValidateProjectFiles(){
-			if(!File.Exists(SolutionFile) && !SyncSolution()){
-				return false;
-			}
+        private bool ValidateProjectFiles()
+        {
+            if (!File.Exists(SolutionFile) && !SyncSolution())
+            {
+                return false;
+            }
 
-			var projectFiles = Directory.GetFiles(projectDirectory, "*.csproj");
-			foreach(var file in projectFiles){
-				ChangeFrameworkVersion(file, _targetVersion);
-			}
-			
-			return true;
-		}
+            var projectFiles = Directory.GetFiles(projectDirectory, "*.csproj");
+            foreach (var file in projectFiles)
+            {
+                ChangeFrameworkVersion(file, _targetVersion);
+            }
 
-		private bool ValidateDotSettings(){
-			var projectFiles = Directory.GetFiles(projectDirectory, "*.csproj");
+            return true;
+        }
 
-			foreach(var file in projectFiles){
-				var dotSettingsFile = file + ".DotSettings";
-				
-				if(File.Exists(dotSettingsFile)){
-					continue;
-				}
-				
-				CreateDotSettingsFile(dotSettingsFile,dotSettingsContent);
-			}
-			
-			return true;
-		}
+        private bool ValidateDotSettings()
+        {
+            var projectFiles = Directory.GetFiles(projectDirectory, "*.csproj");
 
-		private bool ValidateDebugSettings(){
-			return true;
-		}
+            foreach (var file in projectFiles)
+            {
+                var dotSettingsFile = file + ".DotSettings";
 
-		private static void ChangeFrameworkVersion (string projectFile, string targetVersion)
-		{
-			var document = XDocument.Load(projectFile);
-			var frameworkElement = (from el in document.Descendants() 
-			                        where el.Name.LocalName == "TargetFrameworkVersion" 
-			                        select el).FirstOrDefault();
+                if (File.Exists(dotSettingsFile))
+                {
+                    continue;
+                }
 
-			if(frameworkElement == null){
-				return;
-			}
+                CreateDotSettingsFile(dotSettingsFile, dotSettingsContent);
+            }
 
-			frameworkElement.Value = targetVersion;
+            return true;
+        }
 
-			document.Save(projectFile);
-		}
+        private bool ValidateDebugSettings()
+        {
+            return true;
+        }
 
-		private void CreateDotSettingsFile (string dotSettingsFile, string content)
-		{
-			using (var writer = File.CreateText(dotSettingsFile))
-			{
-				writer.Write(content);
-			}		
+        private static void ChangeFrameworkVersion(string projectFile, string targetVersion)
+        {
+            var document = XDocument.Load(projectFile);
+            var frameworkElement = (from el in document.Descendants()
+                where el.Name.LocalName == "TargetFrameworkVersion"
+                select el).FirstOrDefault();
 
-		}
+            if (frameworkElement == null)
+            {
+                return;
+            }
 
-		private static bool SyncSolution(){
-			var T = System.Type.GetType("UnityEditor.SyncVS,UnityEditor");
-			if(T == null){
-				return false;
-			}
+            frameworkElement.Value = targetVersion;
 
-			var syncSolution = T.GetMethod("SyncSolution", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-			syncSolution.Invoke(null, null);
+            document.Save(projectFile);
+        }
 
-			return true;
-		}
+        private void CreateDotSettingsFile(string dotSettingsFile, string content)
+        {
+            using (var writer = File.CreateText(dotSettingsFile))
+            {
+                writer.Write(content);
+            }
+        }
 
-		private readonly string dotSettingsContent = @"<wpf:ResourceDictionary xml:space=""preserve"" xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" xmlns:s=""clr-namespace:System;assembly=mscorlib"" xmlns:ss=""urn:shemas-jetbrains-com:settings-storage-xaml"" xmlns:wpf=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">
+        private static bool SyncSolution()
+        {
+            var T = System.Type.GetType("UnityEditor.SyncVS,UnityEditor");
+            if (T == null)
+            {
+                return false;
+            }
+
+            var syncSolution = T.GetMethod("SyncSolution",
+                System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            syncSolution.Invoke(null, null);
+
+            return true;
+        }
+
+        private readonly string dotSettingsContent =
+            @"<wpf:ResourceDictionary xml:space=""preserve"" xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" xmlns:s=""clr-namespace:System;assembly=mscorlib"" xmlns:ss=""urn:shemas-jetbrains-com:settings-storage-xaml"" xmlns:wpf=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">
                                                                     		<s:String x:Key=""/Default/CodeInspection/CSharpLanguageProject/LanguageLevel/@EntryValue"">CSharp50</s:String></wpf:ResourceDictionary>";
-		
-	}
+    }
 }
