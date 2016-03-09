03c7094d272d5f75554023e8ebf34ece108b0413
yonstorm <juv@users.noreply.github.com>
2016-03-09 10:39:28 +0200

    Rider process creation

---
+++ Assets/Plugins/Editor/ProjectRider/RiderProcess.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace ChaosModel.ProjectRider{
+	internal class RiderProcess {
+		private static RiderInstance riderInstance;
+
+		internal class RiderInstance{
+			private string _appName;
+			private string _baseArgs;
+
+			private RiderInstance(string app, string baseArgs){
+				this._appName = app;
+				this._baseArgs = baseArgs;
+			}
+
+			internal void OpenRider(string args){
+				var process = new Process
+				{
+					StartInfo =
+					{
+						FileName = _appName,
+						Arguments = _baseArgs + " " + args,
+						UseShellExecute = false
+					}
+				};
+				process.Start();
+			}
+		}
+
+		internal static RiderInstance CreateRiderIntance(string solutionFile){
+			var app = "";
+			var baseArgs = solutionFile;
+
+			if(riderInstance == null){
+				riderInstance = new RiderInstance(app,baseArgs);
+			}
+
+			return riderInstance;
+		}
+	}
+}
