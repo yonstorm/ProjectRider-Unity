4896a0353a16e2849a6c8bf76575faad82ba5ae1
yonstorm <juv@users.noreply.github.com>
2016-03-09 09:38:42 +0200

    check dotSettings file exists

--- Assets/Plugins/ProjectRider-Unity/Editor/ProjectValidator.cs
+++ Assets/Plugins/ProjectRider-Unity/Editor/ProjectValidator.cs
@@ -6,6 +6,15 @@ using System.Xml.Linq;
 namespace ChaosModel.ProjectRider
 {
 	internal class ProjectValidator{
+		private static readonly string encodedDotSettingsContent = "PHdwZjpSZXNvdXJjZURpY3Rpb25hcnkgeG1sOnNwYWNlPSIicHJlc2VydmUiIiB4bWxuczp4PSIi" +
+																	"aHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93aW5meC8yMDA2L3hhbWwiIiB4bWxuczpzPSIi" +
+																	"Y2xyLW5hbWVzcGFjZTpTeXN0ZW07YXNzZW1ibHk9bXNjb3JsaWIiIiB4bWxuczpzcz0iInVybjpz" +
+																	"aGVtYXMtamV0YnJhaW5zLWNvbTpzZXR0aW5ncy1zdG9yYWdlLXhhbWwiIiB4bWxuczp3cGY9IiJo" +
+																	"dHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dpbmZ4LzIwMDYveGFtbC9wcmVzZW50YXRpb24i" +
+																	"Ij4gICAgICAgICAgICAgICAgCQk8czpTdHJpbmcgeDpLZXk9IiIvRGVmYXVsdC9Db2RlSW5zcGVj" +
+																	"dGlvbi9DU2hhcnBMYW5ndWFnZVByb2plY3QvTGFuZ3VhZ2VMZXZlbC9ARW50cnlWYWx1ZSIiPkNT" +
+																	"aGFycDUwPC9zOlN0cmluZz48L3dwZjpSZXNvdXJjZURpY3Rpb25hcnk+";
+
 		private string projectDirectory;
 		private string _productName;
 		private string _targetVersion;
@@ -38,7 +47,18 @@ namespace ChaosModel.ProjectRider
 		}
 
 		private bool ValidateDotSettings(){
-
+			var projectFiles = Directory.GetFiles(projectDirectory, "*.csproj");
+			foreach(var file in projectFiles){
+				var dotSettingsFile = file + ".DotSettings";
+				
+				if(File.Exists(dotSettingsFile)){
+					continue;
+				}
+				
+				CreateDotSettingsFile(dotSettingsFile);
+			}
+			
+			return true;
 		}
 
 		private bool ValidateDebugSettings(){
@@ -63,7 +83,12 @@ namespace ChaosModel.ProjectRider
 
 		void CreateDotSettingsFile (string dotSettingsFile)
 		{
-			throw new NotImplementedException ();
+			using (var writer = File.CreateText(dotSettingsFile))
+			{
+				writer.Write("");
+			}
+			
+
 		}
 
 		private static bool SyncSolution(){
