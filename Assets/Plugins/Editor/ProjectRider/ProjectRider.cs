69aef1974bb5d4500cc5e6f1f8e7527ecab2a5db
yonstorm <juv@users.noreply.github.com>
2016-03-09 11:32:14 +0200

    RiderInstance handling integrated

--- Assets/Plugins/Editor/ProjectRider/ProjectRider.cs
+++ Assets/Plugins/Editor/ProjectRider/ProjectRider.cs
@@ -3,16 +3,27 @@ using System.Collections;
 using UnityEditor;
 
 namespace ChaosModel.ProjectRider{
+
 	[InitializeOnLoad]
 	public static class ProjectRider{
+
+		public static string ProjectPath
+		{
+			get { return System.IO.Path.GetDirectoryName(Application.dataPath); }
+		}
+
 		private static ProjectValidator _validator;
+		private static RiderInstance _riderInstance;
 
 		static ProjectRider(){
 			Debug.Log("Rider integration initializing");
 			_validator = new ProjectValidator(ProjectPath,PlayerSettings.productName,"v4.0");
 			if(!_validator.Validate()){
 				Debug.LogError("[ProjectRider] Failed to validate project settings");
+				return;
 			}
+
+			_riderInstance = RiderInstance.CreateRiderInstance(_validator.SolutionFile);
 		}
 
 		[UnityEditor.Callbacks.DidReloadScripts]
@@ -22,12 +33,18 @@ namespace ChaosModel.ProjectRider{
 
 		[UnityEditor.Callbacks.OnOpenAsset]
 		private static bool OnAssetOpened(int instanceId, int line){
-			return false;
-		}
-
-		public static string ProjectPath
-		{
-			get { return System.IO.Path.GetDirectoryName(Application.dataPath); }
+			var selected = EditorUtility.InstanceIDToObject(instanceId);
+			if (!(selected is MonoScript))
+			{
+				return false;
+			}
+			
+			var completeAssetPath = ProjectPath + System.IO.Path.DirectorySeparatorChar + AssetDatabase.GetAssetPath(selected);
+			var args = string.Format(@"{0} --line {1} {2}", _validator.SolutionFile, line, completeAssetPath);
+			
+			_riderInstance.OpenRider(args);
+			
+			return true;
 		}
 
 		public class RiderAssetPostProcessor : AssetPostprocessor
