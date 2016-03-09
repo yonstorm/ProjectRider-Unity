1f57c39d3878a7e0f74950e7df6e7fa0a5fda806
yonstorm <juv@users.noreply.github.com>
2016-03-09 11:31:34 +0200

    Changed app path

--- Assets/Plugins/Editor/ProjectRider/RiderProcess.cs
+++ Assets/Plugins/Editor/ProjectRider/RiderProcess.cs
@@ -1,34 +1,33 @@
 ﻿using System.Diagnostics;
 
 namespace ChaosModel.ProjectRider{
-	internal class RiderProcess {
+	internal class RiderInstance {
 		private static RiderInstance riderInstance;
 
-		internal class RiderInstance{
-			private string _appName;
-			private string _baseArgs;
+		private string _appName;
+		private string _baseArgs;
 
-			private RiderInstance(string app, string baseArgs){
-				this._appName = app;
-				this._baseArgs = baseArgs;
-			}
-
-			internal void OpenRider(string args){
-				var process = new Process
-				{
-					StartInfo =
-					{
-						FileName = _appName,
-						Arguments = _baseArgs + " " + args,
-						UseShellExecute = false
-					}
-				};
-				process.Start();
-			}
+		private RiderInstance(string app, string baseArgs){
+			this._appName = app;
+			this._baseArgs = baseArgs;
 		}
 
-		internal static RiderInstance CreateRiderIntance(string solutionFile){
-			var app = "";
+		internal void OpenRider(string args){
+			var process = new Process
+			{
+				StartInfo =
+				{
+					FileName = _appName,
+					Arguments = _baseArgs + " " + args,
+					UseShellExecute = false
+				}
+			};
+			process.Start();
+		}
+
+
+		internal static RiderInstance CreateRiderInstance(string solutionFile){
+			var app = @"/Applications/Rider EAP.app/Contents/MacOS/rider";
 			var baseArgs = solutionFile;
 
 			if(riderInstance == null){
