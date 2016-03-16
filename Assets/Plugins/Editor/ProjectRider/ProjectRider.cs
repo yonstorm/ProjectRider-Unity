5d141f35a92f9a0e27349c12c9b16ad0173dd27a
yonstorm <juv@users.noreply.github.com>
2016-03-16 10:32:25 +0200

    solution file whitespace handling

--- Assets/Plugins/Editor/ProjectRider/ProjectRider.cs
+++ Assets/Plugins/Editor/ProjectRider/ProjectRider.cs
@@ -47,7 +47,7 @@ namespace ChaosModel.ProjectRider{
 		    }
 			
 			var completeAssetPath = "\"" + ProjectPath + System.IO.Path.DirectorySeparatorChar + AssetDatabase.GetAssetPath(selected) + "\"";
-			var args = string.Format("{0} --line {1} {2}", Validator.SolutionFile, line, completeAssetPath);
+			var args = string.Format("{0} --line {1} {2}", "\"" + Validator.SolutionFile + "\"", line, completeAssetPath);
 			
 			RiderInstance.OpenRider(args);
 			
